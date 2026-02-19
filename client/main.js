0a9b0d361502c1faae2cc5f3f4a936e13b0162ee
troojin <eliasfunnyy@gmail.com>
2026-02-18 23:37:11 -0500

    Delete main.js

--- client/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const token = localStorage.getItem("auth_token");
-
-  if (!token) {
-    window.location.href = "/login";
-  } else {
-    window.location.href = "/home";
-  }
-});
